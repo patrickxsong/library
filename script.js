bbba9f0d450d08a3f157d164a98ae1c97dcb86fe
patrick <patrickjksong@gmail.com>
2022-02-11 10:57:59 -0800

    Reformatted to modular code and fixed missing variable declarations

--- script.js
+++ script.js
@@ -1,103 +1,97 @@
-let myLibrary = [];
+const library = (function() {
+    const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+    const popupForm = document.querySelector(".popup");
+    const closeBtn = document.querySelector(".close");
+    const addBookBtn = document.querySelector(".addBook");
+    const submitBtn = document.querySelector(".submit");
+    const errorElement = document.querySelector("#error-message");
+    const container = document.querySelector(".container");
 
-const popupBtn = document.querySelector(".addBook");
-const popupForm = document.querySelector(".popup");
-const closeBtn = document.querySelector(".close");
-const addBookBtn = document.querySelector(".addBook");
-const submitBtn = document.querySelector(".submit");
-const errorElement = document.querySelector("#error-message");
+    const Book = (title, author, pages, read) => {
+        return {
+            title,
+            author,
+            pages,
+            read
+        };
+    };
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+    const displayBook = book => {
+        myLibrary.push(book)
 
-addBooktoLibrary = book => myLibrary.push(book);
+        let content = document.createElement("div");
+        let remove = document.createElement("button");
+        let toggle = document.createElement("button");
+        let random_color = Math.floor(Math.random()*16777215).toString(16);
+    
+        content.classList.add("card");
+        content.style.backgroundColor = "#" + random_color;
+        content.textContent = book.title;
+        content.id = myLibrary.indexOf(book);
+    
+        remove.classList.add("remove-button");
+        remove.textContent = "Remove"          
+    
+        toggle.classList.add("toggle-button");
+        toggle.textContent = book.read ? "Read" : "Unread";
+        
+        remove.onclick = function () {
+            let idx = remove.parentElement.id;
+            container.removeChild(document.getElementById(idx));
+            myLibrary.splice(idx,1);
+        }
 
-const container = document.querySelector(".container");
+        toggle.onclick = function () {
+            if (book.read) {
+                book.read = false;
+                toggle.textContent = "Unread"
+            } else {
+                book.read = true;
+                toggle.textContent = "Read"
+            }
+        }
 
-displayBooks = () => {
-    myLibrary.forEach(book => display(book));
-};
+        content.appendChild(remove);
+        content.appendChild(toggle);
+        
+        container.appendChild(content);
+    };
 
-display = book => {
-    let content = document.createElement("div");
-    let remove = document.createElement("button");
-    let toggle = document.createElement("button");
+    const theHobbit = Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+    displayBook(theHobbit);
 
-    content.classList.add("card");
-    content.textContent = book.title;
-    content.id = myLibrary.indexOf(book);
+    const getBookInfo = () => myLibrary.forEach(book => console.log(book));    
 
-    remove.classList.add("remove-button");
-    remove.textContent = "Remove"
+    const openForm = () => popupForm.style.display="flex";
+    
+    const closeForm = () => popupForm.style.display="none";
 
-    remove.onclick = function () {
-        let index = remove.parentElement.id;
-        container.removeChild(document.getElementById(index));
-        myLibrary.splice(index,1);
-    }
-
-    content.appendChild(remove);
-
-    toggle.classList.add("toggle-button");
-
-    if (book.read) {
-        toggle.textContent = "Read"
-    } else {
-        toggle.textContent = "Unread"
-    }
-
-    toggle.onclick = function () {
-        if (book.read) {
-            book.read = false;
-            toggle.textContent = "Unread"
-        } else {
-            book.read = true;
-            toggle.textContent = "Read"
+    const submitForm = e => {
+        let messages = [];
+        let newTitle = document.querySelector("#title").value;
+        let newAuthor = document.querySelector("#author").value;
+        let newPages = document.querySelector("#pages").value;
+        let checkRead = document.querySelector("#read").checked;
+        let newBook = Book(newTitle,newAuthor,newPages,checkRead);
+    
+        if (newTitle && newAuthor && newPages > 0) {
+            displayBook(newBook);
+            closeForm();
+            document.querySelector(".form").reset();
+    
+        } else { 
+            messages.push("*All entries must be submitted");
+            e.preventDefault();
+            errorElement.innerText = messages.join(" ");
         }
     }
 
-    content.appendChild(toggle);
-    
-    let random_color = Math.floor(Math.random()*16777215).toString(16);
-    content.style.backgroundColor = "#" + random_color;
-    container.appendChild(content);
-};
+    closeBtn.addEventListener('click', closeForm);
+    addBookBtn.addEventListener('click', openForm);
+    submitBtn.addEventListener('click', submitForm);
 
-addBooktoLibrary(theHobbit);
-displayBooks();
-
-openForm = () => {
-    popupForm.style.display="flex";
-}
-
-closeForm = () => popupForm.style.display="none";
-
-closeBtn.addEventListener('click', closeForm);
-addBookBtn.addEventListener('click', openForm);
-
-submitBtn.addEventListener('click', e => {
-    let messages = [];
-    let newTitle = document.querySelector("#title").value;
-    let newAuthor = document.querySelector("#author").value;
-    let newPages = document.querySelector("#pages").value;
-    let checkRead = document.querySelector("#read").checked;
-    let newBook = new Book(newTitle,newAuthor,newPages,checkRead);
-
-    if (newTitle && newAuthor && newPages > 0) {
-        addBooktoLibrary(newBook);
-        display(newBook);
-        closeForm();
-        document.querySelector(".form").reset();
-
-    } else { 
-        messages.push("*All entries must be submitted");
-        e.preventDefault();
-        errorElement.innerText = messages.join(" ");
-    }
-});
+    return {
+        getBookInfo: getBookInfo
+    };
+})();
